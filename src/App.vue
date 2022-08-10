8b2dca9b1dbbe75212c15b76d19ecb344b93240d
Levi <418448797@qq.com>
2022-08-10 11:18:11 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TestMap></TestMap>
+  </div>
+</template>
+
+<script>
+import TestMap from './components/TestMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TestMap
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
